82fdd6fec6a8c9fe7c0cfbe52203180da5979967
maomao726 <l0980071172@gmail.com>
2023-10-26 07:13:50 +0000

    HW1 finished

--- HW1/part1/vectorOP.cpp
+++ HW1/part1/vectorOP.cpp
@@ -49,9 +49,49 @@ void clampedExpVector(float *values, int *exponents, float *output, int N)
   // Your solution should work for any value of
   // N and VECTOR_WIDTH, not just when VECTOR_WIDTH divides N
   //
-  for (int i = 0; i < N; i += VECTOR_WIDTH)
-  {
-  }
+   __pp_vec_float x;
+   __pp_vec_int y;
+   __pp_mask maskAll = _pp_init_ones(), mask_zero, mask_clamped;
+   __pp_vec_int zero = _pp_vset_int(0);
+   __pp_vec_int one = _pp_vset_int(1);
+   __pp_vec_float clamp = _pp_vset_float(9.999999f);
+
+   if(N % VECTOR_WIDTH != 0)
+   {
+    for(int i = N; i < VECTOR_WIDTH; i++)
+    {
+      values[i] = 0;
+      exponents[i] = 1;
+    }
+   }
+
+   for(int i = 0; i < N; i += VECTOR_WIDTH)
+   {
+    __pp_vec_float result;
+    __pp_mask mask_not_zero;
+    _pp_vload_float(x, values+i, maskAll);
+    _pp_vload_int(y, exponents+i, maskAll);
+
+    _pp_veq_int(mask_zero, y, zero, maskAll);
+    _pp_vset_float(result, 1.f, mask_zero);
+    
+    mask_not_zero = _pp_mask_not(mask_zero);
+    _pp_vmove_float(result, x, mask_not_zero);
+    _pp_vsub_int(y, y, one, maskAll);
+    _pp_vgt_int(mask_not_zero, y, zero, maskAll);
+
+    while(_pp_cntbits(mask_not_zero))
+    {
+      _pp_vmult_float(result, result, x, mask_not_zero);
+      _pp_vsub_int(y, y, one, maskAll);
+      _pp_vgt_int(mask_not_zero, y, zero, maskAll);
+    }
+    
+    _pp_vgt_float(mask_clamped, result, clamp, maskAll);
+    _pp_vset_float(result, 9.999999f, mask_clamped);
+
+    _pp_vstore_float(output+i, result, maskAll);
+   }
 }
 
 // returns the sum of all elements in values
@@ -63,10 +103,21 @@ float arraySumVector(float *values, int N)
   //
   // PP STUDENTS TODO: Implement your vectorized version of arraySumSerial here
   //
-
+  __pp_vec_float x;
+  __pp_mask maskAll = _pp_init_ones();
+  float* result = new float[VECTOR_WIDTH];
+  float sum = 0.f;
   for (int i = 0; i < N; i += VECTOR_WIDTH)
   {
+    _pp_vload_float(x, values+i, maskAll);
+    for(int j = 1; j < VECTOR_WIDTH; j *= 2)
+    {
+      _pp_hadd_float(x, x);
+      _pp_interleave_float(x, x);
+    }
+    _pp_vstore_float(result, x, maskAll);
+    sum += result[0];
   }
 
-  return 0.0;
+  return sum;
 }
--- HW1/part2/test2.cpp
+++ HW1/part2/test2.cpp
@@ -7,7 +7,6 @@ void test2(float *__restrict a, float *__restrict b, float *__restrict c, int N)
   __builtin_assume(N == 1024);
   a = (float *)__builtin_assume_aligned(a, 16);
   b = (float *)__builtin_assume_aligned(b, 16);
-  c = (float *)__builtin_assume_aligned(c, 16);
 
   fasttime_t time1 = gettime();
   for (int i = 0; i < I; i++)
@@ -15,8 +14,9 @@ void test2(float *__restrict a, float *__restrict b, float *__restrict c, int N)
     for (int j = 0; j < N; j++)
     {
       /* max() */
-      if (b[j] > a[j]) c[j] = b[j];
-      else c[j] = a[j];
+      c[j] = a[j];
+      if (b[j] > a[j])
+        c[j] = b[j];
     }
   }
 
